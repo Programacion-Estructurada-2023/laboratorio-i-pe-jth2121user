487a379bdd8b8f91a12a4de8baa04c30184bd1d2
jth2121 <judithdelciddelao@gmail.com>
2023-08-21 14:23:37 -0600

    Laboratorio1

---
+++ Laboratorio1_U20220220.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MyApp// Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+         static void Main(string[] args)
+        {
+            //Declarando de variables de tipo texto, capturando su valor desde la consola.
+            string? Departamento;
+            string? Municipio;
+            string? Ciudad;
+            
+            //Declarando de variables de tipo texto,  asignando su valor desde la declaración.
+            string? Colonia = "La Caridad";
+            string? Color_casa = "blanco";
+
+            //Declaracion de variables de tipo enteras, asignando su valor desde consola
+            int Años_vividos;
+            int Edad;
+
+            //Declarando de variables de tipo date time,  asignando su valor desde la declaración.
+            DateOnly Fecha_nacimiento = new DateOnly(2002,03,21);
+            DateTime Fecha_actual = DateTime.Now;   
+        }
+    }
+}
